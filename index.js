6df1a303df8a300394c0aae6423b1267859c0a32
andrjre <andrjre@gmail.com>
2025-04-03 10:06:50 +0100

    Got 2 boxes.

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let div = document.getElementById("box");
+div.style.width = "50px";
+div.style.height = "50px";
+div.style.background = "white";
+div.style.border = "2px solid black";
+
+for(i = 1; i <100 ; i++){
+    
+}
