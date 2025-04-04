de89b9f4cc3c255b7d0b5d0e4e9e47f013615d9a
andrjre <andrjre@gmail.com>
2025-04-04 15:20:21 +0100

    Made horizontal grid generation and hover.

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 
-let div2 = document.createElement("div")
 
-let div = document.getElementById("box");
-div.style.width = "50px";
-div.style.height = "50px";
-div.style.background = "white";
-div.style.border = "2px solid black";
+let sixteenBtn = document.getElementById("sixteenBtn");
+let boxContainer = document.getElementById("boxContainer")
+    
 
-let sixteen = document.getElementById("sixteen");
 
-sixteen.onclick = function(){
+
+sixteenBtn.onclick = function(){
+    
+    //when button is clicked //
     console.log("16px")
+
+    
+
+    //runs loop 
+    for(i = 0; i<16 ; i++){
+
+    //creates box//
     let boxCreate = document.createElement("div")
-    boxCreate.style.background = "red";
-    boxCreate.style.width = "20px";
-    boxCreate.style.height = "20px";
-    document.body.appendChild(boxCreate)
-}
+    boxCreate.style.border = "solid black 2px";
+    boxCreate.id = "hi";
+    boxContainer.appendChild(boxCreate)
 
-
-for(i = 1; i <100 ; i++){
+    }
     
 }
+
+
+
