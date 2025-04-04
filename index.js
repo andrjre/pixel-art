89e211ade943331e299821f478d81dcd5c687125
andrjre <andrjre@gmail.com>
2025-04-04 16:01:58 +0100

    Comments.

--- index.js
+++ index.js
@@ -9,26 +9,30 @@ sixteenBtn.onclick = function(){
     
     //when button is clicked //
     console.log("16px")
-
     
 
     //runs loop 
-    for(i = 0; i<256 ; i++){
+    for(i = 0; i<4096 ; i++){
 
     //creates box//
     let boxCreate = document.createElement("div")
-    boxCreate.style.border = "solid black 1px";
     boxCreate.classList.add("createdBoxes");
     boxContainer.appendChild(boxCreate)
     }
 
-    //fill in on click//
-    let createdBoxes = document.getElementsByClassName("createdBoxes")
-    for(i = 0; i < 16 ; i++)
-    createdBoxes.onclick = function(){
-        console.log("hello")
-    }   
-    
+
+
 }
+
+createdBoxes = document.getElementsByClassName("createdBoxes")
+
+createdBoxes.onclick = function(){
+    console.log("hello")
+}
+
+
+
+
+// find out how to disable the button after being pressed//
 
 
