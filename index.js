e53bbe155640df1e0fe9aa9c24dd10acc278ae0c
andrjre <andrjre@gmail.com>
2025-04-05 13:25:03 +0100

    Button to double number of pixels.

--- index.js
+++ index.js
@@ -2,25 +2,30 @@
 
 let sixteenBtn = document.getElementById("sixteenBtn");
 let boxContainer = document.getElementById("boxContainer")
+let gridSize = 1;
 
 
 
 sixteenBtn.onclick = function(){
     
     //when button is clicked //
-    console.log("16px")
-    
+
+    console.log(gridSize)
+    gridSize = gridSize * 2;
+    let total = gridSize ** 2;
+    boxContainer.innerHTML = "" // sets the boxcontainer to 0 every click, resetting the boxes//
 
     //runs loop 
-    for(i = 0; i<4096 ; i++){
+    for(i = 0; i<total ; i++){
 
     //creates box//
+
     let boxCreate = document.createElement("div")
     boxCreate.classList.add("createdBoxes");
     boxContainer.appendChild(boxCreate)
+    boxContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
+    boxContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
     }
-
-
 
 }
 
