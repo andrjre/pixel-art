44ec4f8d3d32408f269d00012bf318a5be87b71b
andrjre <andrjre@gmail.com>
2025-04-06 19:24:58 +0100

    Locked button when pixel count is too high.

--- index.js
+++ index.js
@@ -16,27 +16,34 @@ increaseBtn.onclick = function(){
 
     pixelCount.innerHTML = `${gridSize} x ${gridSize}`;
     let total = gridSize ** 2;
-    boxContainer.innerHTML = "" // sets the boxcontainer to 0 every click, resetting the boxes//
-    
+    boxContainer.innerHTML = ""     // sets the boxcontainer to 0 every click, resetting the boxes//
+    let boxCreate;
+
     //runs loop 
     for(i = 0; i<total ; i++){
 
     //creates box//
-
-    let boxCreate = document.createElement("div")
+    boxCreate = document.createElement("div")
     boxCreate.classList.add("createdBoxes");
     boxContainer.appendChild(boxCreate)
     boxContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
     boxContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
-    
+
+    if (gridSize >= 64){
+        document.getElementById("increaseBtn").disabled = true;
+    }
     if (gridSize >64){
         console.log("too big !")
     }
     else if(gridSize >= 32){
         boxCreate.style.border= "1px solid rgba(0, 0, 0, .5)"   
-        }
     }
+    }
+
+   
+    
 }
+
 
 
 // find out how to disable the button after being pressed//
