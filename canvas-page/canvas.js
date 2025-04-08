d0e5966ac88a649bec041104d812ec1a76662d3e
andrjre <andrjre@gmail.com>
2025-04-08 21:36:19 +0100

    Creates grid in new page almost.

--- canvas-page/canvas.js
+++ canvas-page/canvas.js
@@ -9,103 +9,13 @@ let total = 2;
 console.log(gridSize)
 document.getElementById("decreaseBtn").disabled = true;
 
-localStorage.getItem("gridSize");
+let newGrid = localStorage.getItem("gridSize");
 console.log(localStorage)
 
+        boxContainer.style.gridTemplateColumns = `repeat(${newGrid},1fr)`
+        boxContainer.style.gridTemplateRows = `repeat(${newGrid}, 1fr)`
 
-increaseBtn.onclick = function(){
     
-    gridSize = gridSize * 2;
-    console.log(gridSize)
-
-    pixelCount.innerHTML = `${gridSize} x ${gridSize}`;
-    let total = gridSize ** 2;
-    boxContainer.innerHTML = "";
-
-    for(i = 0; i<total ; i++){
-        boxCreate = document.createElement("div")
-        boxCreate.classList.add("createdBoxes");
-        boxContainer.appendChild(boxCreate)
-        boxContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
-        boxContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
-        if(gridSize >= 32){
-            boxCreate.style.border= "1px solid rgba(0, 0, 0, .5)"   
-        }
-        if(gridSize >= 64){
-            boxCreate.style.border= "1px solid rgba(0, 0, 0, .25)"   
-        } 
-    }
-
-    if (gridSize >= 64){
-        document.getElementById("increaseBtn").disabled = true;
-    }
-    else if (gridSize <= 64){
-        document.getElementById("increaseBtn").disabled = false;
-    }
-
-    if (gridSize > 4){
-        document.getElementById("decreaseBtn").disabled = false;
-    }
-    else if(gridSize <= 4){
-        document.getElementById("decreaseBtn").disabled = true;
-    }
-    
-    let clickBoxes = document.getElementsByClassName("createdBoxes");
-
-    for (let i = 0; i < clickBoxes.length; i++) {
-        clickBoxes[i].onclick = function() {
-            clickBoxes[i].style.background = "purple";
-        }
-    }
-}
-
-decreaseBtn.onclick = function(){
-
-    gridSize = gridSize / 2;
-    console.log(gridSize)
-
-    pixelCount.innerHTML = `${gridSize} x ${gridSize}`;
-    let total = gridSize ** 2;
-    boxContainer.innerHTML = ""  
-
-
-    for(i = 0; i<total ; i++){
-        boxCreate = document.createElement("div")
-        boxCreate.classList.add("createdBoxes");
-        boxContainer.appendChild(boxCreate)
-        boxContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
-        boxContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
-        if(gridSize >= 32){
-            boxCreate.style.border= "1px solid rgba(0, 0, 0, .5)"   
-        }
-        if(gridSize >= 64){
-            boxCreate.style.border= "1px solid rgba(0, 0, 0, .25)"   
-        } 
-    }
-
-    if (gridSize > 4){
-        document.getElementById("decreaseBtn").disabled = false;
-    }
-    else if(gridSize <= 4){
-        document.getElementById("decreaseBtn").disabled = true;
-    }
-    if (gridSize >= 64){
-        document.getElementById("increaseBtn").disabled = true;
-        console.log(">=64")
-    }
-    else if (gridSize <= 64){
-        document.getElementById("increaseBtn").disabled = false;
-    }
-
-    let clickBoxes = document.getElementsByClassName("createdBoxes");
-
-    for (let i = 0; i < clickBoxes.length; i++) {
-        clickBoxes[i].onclick = function() {
-            clickBoxes[i].style.background = "purple";
-        }
-    }
-   
-}
 
 // when go button is pressed
 // remove all other features
